c81fea2114ecb89cacfc16603885dda92e855526
vsirineni2 <sirineni.vamsi@gmail.com>
2022-07-22 14:08:17 -0400

    Timer Mechanishms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,17 +9,74 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = None
+marks =""
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global marks
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    marks = ""
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    WORK_SEC = WORK_MIN * 60
+    SHORT_BREAK_SEC = SHORT_BREAK_MIN * 60
+    LONG_BREAK_SEC = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_SEC)
+        title_label.config(text="Break", bg=RED, fg=GREEN)
+        window.config(bg=RED)
+        canvas.config(bg=RED)
+
+
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_SEC)
+        title_label.config(text="Break", bg=PINK, fg=RED)
+        window.config(bg=PINK)
+        canvas.config(bg=PINK)
+
+    else:
+        count_down(WORK_SEC)
+        title_label.config(text="Work", bg=GREEN, fg=RED)
+        window.config(bg=GREEN)
+        canvas.config(bg=GREEN)
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        global marks
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            marks += "✓"
+
+        checkmark_label.config(text=marks)
+
+    
+        
+
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -27,7 +85,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 title_label = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 35), fg=GREEN)
 title_label.grid(column=1, row=0)
 
-checkmark_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+checkmark_label = Label(bg=YELLOW, fg=GREEN)
 checkmark_label.grid(column=1, row=3)
 
 
@@ -38,13 +96,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
+
 
 #Buttons
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 
